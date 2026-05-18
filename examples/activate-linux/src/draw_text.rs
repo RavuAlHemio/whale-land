b522e15008f7eb6f28c602d6adc4f816a37da099
Ondřej Hošek <ondra.hosek@gmail.com>
2026-05-18 04:05:46 +0200

    activate-linux: render and display the message

---
+++ examples/activate-linux/src/draw_text.rs
@@ -0,0 +1,172 @@
+use std::collections::HashMap;
+
+use swash::FontRef;
+use swash::scale::{Render, ScaleContext, Source, StrikeWith};
+use swash::shape::ShapeContext;
+use swash::zeno::Vector;
+
+
+const FONT_DATA: &[u8] = include_bytes!("../segoeuil.ttf");
+
+const UPPER_TEXT: &str = "Activate Linux";
+const UPPER_SIZE_PT: f32 = 28.0;
+const UPPER_ADD_SPACING: f32 = 0.8;
+
+const LOWER_TEXT: &str = "Go to Settings to activate Linux.";
+const LOWER_SIZE_PT: f32 = 21.0;
+const LOWER_ADD_SPACING: f32 = 0.4;
+
+const BASELINE_SPACING: f32 = 38.0;
+
+
+#[derive(Clone, Debug, Default, Eq, PartialEq)]
+pub struct Image {
+    pub width: u32,
+    pub height: u32,
+    pub pixels: HashMap<(u32, u32), u8>,
+}
+impl Image {
+    pub fn max_y_coordinate(&self) -> u32 {
+        self.pixels
+            .keys()
+            .map(|(_x, y)| *y)
+            .max()
+            .unwrap_or(0)
+    }
+
+    pub fn to_white_argb_le(&self) -> Vec<u8> {
+        let pixel_color_count: usize = (self.width * self.height * 4).try_into().unwrap();
+        let mut pixels = Vec::with_capacity(pixel_color_count);
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let alpha_value = self.pixels.get(&(x, y))
+                    .copied()
+                    .unwrap_or(0x00);
+                let argb =
+                    ((alpha_value as u32) << 24)
+                    | ((alpha_value as u32) << 16)
+                    | ((alpha_value as u32) <<  8)
+                    | ((alpha_value as u32) <<  0);
+                pixels.extend_from_slice(&argb.to_le_bytes());
+            }
+        }
+        pixels
+    }
+}
+
+
+fn draw_string(font: FontRef<'_>, text: &str, size: f32, add_spacing: f32) -> Image {
+    // load font
+    let metrics = font.metrics(&[]);
+    let ascender_px_f32 = metrics.ascent * size / f32::from(metrics.units_per_em);
+    let ascender_px: i32 = ascender_px_f32.ceil() as i32;
+
+    // shape text
+    let mut shape_ctx = ShapeContext::new();
+    let mut shaper = shape_ctx.builder(font)
+        .size(size)
+        .build();
+    shaper.add_str(text);
+    let mut glyphs = Vec::new();
+    shaper.shape_with(|cluster| {
+        for glyph in cluster.glyphs {
+            glyphs.push(*glyph);
+        }
+    });
+
+    // render text
+    let mut context = ScaleContext::new();
+    let mut scaler = context.builder(font)
+        .size(size)
+        .hint(false)
+        .build();
+    let mut renderer = Render::new(&[
+        Source::ColorOutline(0),
+        Source::ColorBitmap(StrikeWith::BestFit),
+        Source::Outline,
+    ]);
+    let mut pixel_values: HashMap<(u32, u32), u8> = HashMap::new();
+    let mut pos_x: f32 = 0.0;
+    for glyph in &glyphs {
+        let pos_x_int: u32 = pos_x.trunc() as u32;
+        let pos_x_frac = pos_x.fract();
+        renderer.offset(Vector::new(pos_x_frac, 0.0));
+        let img = renderer.render(&mut scaler, glyph.id)
+            .expect("failed to render glyph");
+
+        for y in 0..img.placement.height {
+            for x in 0..img.placement.width {
+                let i: usize = (y * img.placement.width + x).try_into().unwrap();
+                let b = img.data[i];
+                if b == 0 {
+                    continue;
+                }
+                let actual_x: u32 = match (img.placement.left + i32::try_from(pos_x_int + x).unwrap()).try_into() {
+                    Ok(ax) => ax,
+                    Err(_) => continue,
+                };
+                let actual_y: u32 = match (ascender_px - img.placement.top + i32::try_from(y).unwrap()).try_into() {
+                    Ok(ay) => ay,
+                    Err(_) => continue,
+                };
+                let pixel_ref = pixel_values
+                    .entry((actual_x, actual_y))
+                    .or_insert(0);
+                *pixel_ref = pixel_ref.saturating_add(b);
+            }
+        }
+
+        pos_x += glyph.advance + add_spacing;
+    }
+
+    let final_width = pos_x.ceil() as u32;
+    let mut ret = Image {
+        width: final_width,
+        height: 0,
+        pixels: pixel_values,
+    };
+    ret.height = ret.max_y_coordinate();
+    ret
+}
+
+
+pub fn draw_text(scale: f32) -> Image {
+    let font = FontRef::from_index(FONT_DATA, 0)
+        .expect("failed to load font");
+
+    let upper_image = draw_string(
+        font,
+        UPPER_TEXT,
+        UPPER_SIZE_PT * scale,
+        UPPER_ADD_SPACING,
+    );
+    let lower_image = draw_string(
+        font,
+        LOWER_TEXT,
+        LOWER_SIZE_PT * scale,
+        LOWER_ADD_SPACING,
+    );
+
+    let scaled_baseline_spacing: u32 = (BASELINE_SPACING * scale).round() as u32;
+
+    let mut all_text_image = Image {
+        width: 0,
+        height: 0,
+        pixels: HashMap::new(),
+    };
+    for ((x, y), value) in upper_image.pixels {
+        let pixel_ref = all_text_image.pixels
+            .entry((x, y))
+            .or_insert(0);
+        *pixel_ref = pixel_ref.saturating_add(value);
+    }
+    for ((x, y), value) in lower_image.pixels {
+        let pixel_ref = all_text_image.pixels
+            .entry((x, y + scaled_baseline_spacing))
+            .or_insert(0);
+        *pixel_ref = pixel_ref.saturating_add(value);
+    }
+    all_text_image.width = upper_image.width.max(lower_image.width);
+    all_text_image.height = all_text_image.max_y_coordinate();
+    all_text_image
+}
--- examples/activate-linux/src/main.rs
+++ examples/activate-linux/src/main.rs
@@ -1,27 +1,55 @@
+mod draw_text;
+
+
+use std::os::fd::AsRawFd;
 use std::sync::LazyLock;
 
 use async_trait::async_trait;
 use tokio::sync::Mutex;
+use whale_land::shared_memory::SharedMemoryObject;
 use whale_land::{Connection, Error, NewObject, NewObjectId, ObjectId, Packet};
 use whale_land::protocol::EventHandler;
 use whale_land::protocol::wayland::{
-    wl_compositor_v7_request_proxy, wl_registry_v1_event_handler, wl_registry_v1_request_proxy,
-    wl_surface_v7_request_proxy,
+    wl_buffer_v1_event_handler, wl_compositor_v7_request_proxy, wl_output_v4_transform_u32, wl_registry_v1_event_handler, wl_registry_v1_request_proxy, wl_shm_pool_v2_request_proxy, wl_shm_v2_event_handler, wl_shm_v2_format_u32, wl_shm_v2_request_proxy, wl_surface_v7_event_handler, wl_surface_v7_request_proxy
 };
 use whale_land::protocol::wlr_layer_shell_unstable_v1::{
     zwlr_layer_shell_v1_v5_layer_u32, zwlr_layer_shell_v1_v5_request_proxy,
+    zwlr_layer_surface_v1_v5_event_handler, zwlr_layer_surface_v1_v5_request_proxy,
 };
-use tracing::{debug, instrument};
+use tracing::{debug, info, instrument};
 
 
-#[derive(Clone, Copy, Debug, Default, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Clone, Copy, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct DisplayState {
     pub compositor_oid: Option<ObjectId>,
     pub layer_shell_oid: Option<ObjectId>,
+    pub shm_oid: Option<ObjectId>,
+    pub surface_oid: Option<ObjectId>,
+    pub layer_surface_oid: Option<ObjectId>,
+    pub preferred_scale: i32,
+    pub preferred_transform: wl_output_v4_transform_u32,
+    pub width: u32,
+    pub height: u32,
+}
+impl Default for DisplayState {
+    fn default() -> Self {
+        Self {
+            compositor_oid: None,
+            layer_shell_oid: None,
+            shm_oid: None,
+            surface_oid: None,
+            layer_surface_oid: None,
+            preferred_scale: 1,
+            preferred_transform: wl_output_v4_transform_u32::NORMAL,
+            width: 0,
+            height: 0,
+        }
+    }
 }
 static DISPLAY_STATE: LazyLock<Mutex<DisplayState>> = LazyLock::new(|| Mutex::new(DisplayState::default()));
 
 
+#[derive(Debug)]
 struct RegistryHandler;
 impl RegistryHandler {
     async fn surface_creation_rigmarole(&self, connection: &Connection) {
@@ -31,6 +59,9 @@ impl RegistryHandler {
                 else { return };
             let Some(lsh) = display_state_guard.layer_shell_oid
                 else { return };
+            if display_state_guard.shm_oid.is_none() {
+                return;
+            }
             (comp, lsh)
         };
 
@@ -40,6 +71,11 @@ impl RegistryHandler {
             connection.downgrade(),
         );
         let surface_oid = connection.get_and_increment_next_object_id();
+        debug!("surface OID is {}", surface_oid.0);
+        {
+            let mut display_state_guard = DISPLAY_STATE.lock().await;
+            display_state_guard.surface_oid = Some(surface_oid);
+        }
         compositor.send_create_surface(NewObjectId(surface_oid))
             .await.expect("failed to create surface");
 
@@ -49,6 +85,11 @@ impl RegistryHandler {
             connection.downgrade(),
         );
         let layer_surface_oid = connection.get_and_increment_next_object_id();
+        debug!("layer surface OID is {}", layer_surface_oid.0);
+        {
+            let mut display_state_guard = DISPLAY_STATE.lock().await;
+            display_state_guard.layer_surface_oid = Some(layer_surface_oid);
+        }
         layer_shell.send_get_layer_surface(
             NewObjectId(layer_surface_oid),
             Some(surface_oid),
@@ -57,7 +98,10 @@ impl RegistryHandler {
             "activation",
         ).await.expect("failed to promote surface to layer surface");
 
-        // TODO: add event handler
+        connection.register_handler(surface_oid, Box::new(SurfaceHandler))
+            .await;
+        connection.register_handler(layer_surface_oid, Box::new(LayerSurfaceHandler))
+            .await;
 
         // commit it once while it's empty
         let surface = wl_surface_v7_request_proxy::new(
@@ -78,10 +122,10 @@ impl wl_registry_v1_event_handler for RegistryHandler {
     #[instrument(skip_all)]
     async fn handle_global(
         &self,
-        connection: &whale_land::Connection,
-        packet: whale_land::Packet,
+        connection: &Connection,
+        packet: Packet,
         name: u32,
-        interface: ::std::string::String,
+        interface: String,
         version: u32,
     ) {
         debug!("{} is {:?} version {}", name, interface, version);
@@ -94,6 +138,7 @@ impl wl_registry_v1_event_handler for RegistryHandler {
         if interface == "wl_compositor" {
             // yeah, I want that one
             let compositor_oid = connection.get_and_increment_next_object_id();
+            debug!("compositor OID is {}", compositor_oid.0);
             registry.send_bind(
                 name,
                 NewObject {
@@ -111,6 +156,7 @@ impl wl_registry_v1_event_handler for RegistryHandler {
         } else if interface == "zwlr_layer_shell_v1" {
             // I want this one too
             let layer_shell_oid = connection.get_and_increment_next_object_id();
+            debug!("layer shell OID is {}", layer_shell_oid.0);
             registry.send_bind(
                 name,
                 NewObject {
@@ -125,18 +171,245 @@ impl wl_registry_v1_event_handler for RegistryHandler {
             drop(display_state_guard);
 
             self.surface_creation_rigmarole(connection).await;
+        } else if interface == "wl_shm" {
+            // gimme
+            let shm_oid = connection.get_and_increment_next_object_id();
+            debug!("shm OID is {}", shm_oid.0);
+            connection.register_handler(shm_oid, Box::new(SharedMemoryHandler)).await;
+            registry.send_bind(
+                name,
+                NewObject {
+                    object_id: shm_oid,
+                    interface: interface.to_owned(),
+                    interface_version: version,
+                },
+            ).await.expect("failed to bind to shared memory provider");
+
+            let mut display_state_guard = DISPLAY_STATE.lock().await;
+            display_state_guard.shm_oid = Some(shm_oid);
+            drop(display_state_guard);
+
+            self.surface_creation_rigmarole(connection).await;
         }
     }
 
     #[instrument(skip_all)]
     async fn handle_global_remove(
         &self,
-        connection: &whale_land::Connection,
-        packet: whale_land::Packet,
+        connection: &Connection,
+        packet: Packet,
         name: u32,
     ) {
         let _ = (connection, packet);
         debug!("{} is gone", name);
+    }
+}
+
+
+#[derive(Debug)]
+struct SurfaceHandler;
+#[async_trait]
+impl EventHandler for SurfaceHandler {
+    async fn handle_event(&self, connection: &Connection, packet: Packet) -> Result<(), Error> {
+        wl_surface_v7_event_handler::handle_event(self, connection, packet).await
+    }
+}
+impl wl_surface_v7_event_handler for SurfaceHandler {
+    #[instrument(skip(connection, packet))]
+    async fn handle_enter(
+        &self,
+        connection: &Connection,
+        packet: Packet,
+        output: Option<ObjectId>,
+    ) {
+        info!("enter");
+    }
+
+    #[instrument(skip(connection, packet))]
+    async fn handle_leave(
+        &self,
+        connection: &Connection,
+        packet: Packet,
+        output: Option<ObjectId>,
+    ) {
+        info!("leave");
+    }
+
+    #[instrument(skip(connection, packet))]
+    async fn handle_preferred_buffer_scale(
+        &self,
+        connection: &Connection,
+        packet: Packet,
+        factor: i32,
+    ) {
+        info!("preferred_buffer_scale");
+        let mut state_guard = DISPLAY_STATE.lock().await;
+        state_guard.preferred_scale = factor;
+    }
+
+    #[instrument(skip(connection, packet))]
+    async fn handle_preferred_buffer_transform(
+        &self,
+        connection: &Connection,
+        packet: Packet,
+        transform: u32,
+    ) {
+        info!("preferred_buffer_transform");
+        let mut state_guard = DISPLAY_STATE.lock().await;
+        state_guard.preferred_transform = transform.into();
+    }
+}
+
+
+#[derive(Debug)]
+struct LayerSurfaceHandler;
+#[async_trait]
+impl EventHandler for LayerSurfaceHandler {
+    async fn handle_event(&self, connection: &Connection, packet: Packet) -> Result<(), Error> {
+        zwlr_layer_surface_v1_v5_event_handler::handle_event(self, connection, packet).await
+    }
+}
+impl zwlr_layer_surface_v1_v5_event_handler for LayerSurfaceHandler {
+    #[instrument(skip(connection, packet))]
+    async fn handle_configure(
+        &self,
+        connection: &Connection,
+        packet: Packet,
+        serial: u32,
+        width: u32,
+        height: u32,
+    ) {
+        let mut state_guard = DISPLAY_STATE.lock().await;
+        state_guard.width = width;
+        state_guard.height = height;
+
+        // acknowledge the configuration
+        let layer_surface = zwlr_layer_surface_v1_v5_request_proxy::new(
+            state_guard.layer_surface_oid.unwrap(),
+            connection.downgrade(),
+        );
+        layer_surface.send_ack_configure(serial)
+            .await.expect("failed to send configuration acknowledgement");
+
+        // calculate the surface
+        let text_image = crate::draw_text::draw_text(state_guard.preferred_scale as f32);
+        let text_image_argb = text_image.to_white_argb_le();
+
+        // create a shared memory segment
+        let shared_memory_length: i32 = text_image_argb.len().try_into().unwrap();
+        let mut shared_memory = SharedMemoryObject::new_anonymous()
+            .expect("failed to create shared memory segment");
+        shared_memory.set_length(shared_memory_length.into())
+            .expect("failed to set shared memory length");
+
+        // make a shmem pool out of it
+        let shm = wl_shm_v2_request_proxy::new(
+            state_guard.shm_oid.unwrap(),
+            connection.downgrade(),
+        );
+        let shm_pool_id = connection.get_and_increment_next_object_id();
+        debug!("shm pool OID is {}", shm_pool_id.0);
+        shm.send_create_pool(
+            NewObjectId(shm_pool_id),
+            shared_memory.as_raw_fd(),
+            shared_memory_length,
+        )
+            .await.expect("failed to create shared memory pool");
+
+        // take a buffer from the pool
+        let shm_pool = wl_shm_pool_v2_request_proxy::new(
+            shm_pool_id,
+            connection.downgrade(),
+        );
+        let buffer_oid = connection.get_and_increment_next_object_id();
+        debug!("buffer OID is {}", buffer_oid.0);
+        connection.register_handler(buffer_oid, Box::new(BufferHandler)).await;
+        shm_pool.send_create_buffer(
+            NewObjectId(buffer_oid),
+            0,
+            text_image.width.try_into().unwrap(),
+            text_image.height.try_into().unwrap(),
+            i32::try_from(text_image.width).unwrap() * 4,
+            wl_shm_v2_format_u32::ARGB8888.into(),
+        )
+            .await.expect("failed to create shared memory buffer");
+
+        // write the image into the buffer
+        {
+            let shared_mapping = shared_memory.map_read_write(..)
+                .expect("failed to map shared memory");
+            let shared_mapping_slice = unsafe {
+                std::slice::from_raw_parts_mut(
+                    shared_mapping.as_mut_ptr() as *mut u8,
+                    text_image_argb.len(),
+                )
+            };
+            shared_mapping_slice.copy_from_slice(&text_image_argb);
+        }
+
+        // attach the buffer to the surface
+        let surface = wl_surface_v7_request_proxy::new(
+            state_guard.surface_oid.unwrap(),
+            connection.downgrade(),
+        );
+        surface.send_attach(
+            Some(buffer_oid),
+            0,
+            0,
+        )
+            .await.expect("failed to attach buffer to surface");
+        surface.send_commit()
+            .await.expect("failed to commit buffer");
+    }
+
+    #[instrument(skip(connection, packet))]
+    async fn handle_closed(
+        &self,
+        connection: &Connection,
+        packet: Packet,
+    ) {
+        info!("closed");
+    }
+}
+
+
+#[derive(Debug)]
+struct SharedMemoryHandler;
+#[async_trait]
+impl EventHandler for SharedMemoryHandler {
+    async fn handle_event(&self, connection: &Connection, packet: Packet) -> Result<(), Error> {
+        wl_shm_v2_event_handler::handle_event(self, connection, packet).await
+    }
+}
+impl wl_shm_v2_event_handler for SharedMemoryHandler {
+    #[instrument(skip(connection, packet))]
+    async fn handle_format(
+        &self,
+        connection: &Connection,
+        packet: Packet,
+        format: u32,
+    ) {
+        debug!("supported pixel buffer format: {}", format);
+    }
+}
+
+
+#[derive(Debug)]
+struct BufferHandler;
+#[async_trait]
+impl EventHandler for BufferHandler {
+    async fn handle_event(&self, connection: &Connection, packet: Packet) -> Result<(), Error> {
+        wl_buffer_v1_event_handler::handle_event(self, connection, packet).await
+    }
+}
+impl wl_buffer_v1_event_handler for BufferHandler {
+    #[instrument(skip(connection, packet))]
+    async fn handle_release(
+        &self,
+        connection: &Connection,
+        packet: Packet,
+    ) {
+        info!("buffer released");
     }
 }
 
@@ -152,6 +425,7 @@ async fn run() {
 
     // make a registry
     let registry_oid = wl.get_and_increment_next_object_id();
+    debug!("registry OID is {}", registry_oid.0);
     wl.register_handler(registry_oid, Box::new(RegistryHandler)).await;
     let display = wl.get_display_proxy();
     display.send_get_registry(NewObjectId(registry_oid))
